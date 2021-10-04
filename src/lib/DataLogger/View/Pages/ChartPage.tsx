fec76c2aa9e3cec7478f699b6c5fcff8ca4ad7be
sz2 <sugiura.k.nigo@gmail.com>
2021-10-05 01:07:31 +0900

    Code ChartPage.

---
+++ src/lib/DataLogger/View/Pages/ChartPage.tsx
@@ -0,0 +1,103 @@
+/* 
+ * The MIT License
+ *
+ * Copyright 2021 sz2.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import { useState, VoidFunctionComponent } from "react";
+import * as Echarts from 'echarts'
+import ReactEcharts from 'echarts-for-react'
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export const ChartPage : VoidFunctionComponent = () => 
+{
+    const [chartOption, setChartOption] = useState<Echarts.EChartOption>();
+    
+    const handleGetData = async () =>
+    {
+        const res = await fetch("/api/store");
+        const dataStore: { time: number[], value: { [key: string]: number[] } } = await res.json();
+    
+        const yAxisOption: Echarts.EChartOption.YAxis[] = [];
+        const seriesOption: Echarts.EChartOption.Series[] = [];
+        let axisIndex = 0;
+
+        for (let code in dataStore.value) {
+            yAxisOption.push({ type: 'value', name: code });
+            seriesOption.push({ data: dataStore.value[code], type: 'line', yAxisIndex: axisIndex });
+            axisIndex++;
+        }
+
+        if(axisIndex == 0) // No data
+        {
+            setChartOption(undefined);
+            return;            
+        }
+
+        const option: Echarts.EChartOption = {
+            tooltip: {
+                show: true,
+                trigger: 'axis'//,
+                //position: function (pt) {
+                //    return [pt[0], '10%'];
+                //}
+            },
+            xAxis: {
+                type: 'category',
+                data: dataStore.time
+            },
+            yAxis: yAxisOption,
+            series: seriesOption
+            /*
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: dataStore.value,
+                type: 'line'
+            }]
+            */
+        };
+        
+        setChartOption(option);
+    }
+ 
+    const chartElem = (chartOption === undefined) ? 
+                    (<div>Data is not available.</div>) 
+                    :
+                    (<ReactEcharts
+                        option={chartOption}
+                        notMerge={true}
+                        lazyUpdate={true}
+                        theme={"theme_name"}
+                        //onChartReady={this.onChartReadyCallback}
+                        //onEvents={EventsDict}
+                        //opts={ }
+                    />);
+        
+    return(
+        <>
+            {chartElem}
+            <Button variant="primary" onClick={handleGetData}>Get data</Button>
+        </>
+    )
+}
